25b4a5c89801275b7993d68f780988a9078c0580
Shane Wilson <shane.wilson@oicr.on.ca>
2016-10-19 16:55:15 -0400

    :recycle: update publish change

--- packages/buildjs/bin/release/publish.js
+++ packages/buildjs/bin/release/publish.js
@@ -2,33 +2,15 @@ const execa = require('execa');
 const UpdaterRenderer = require('listr-update-renderer');
 const Listr = require('listr');
 
-const config = require('@ncigdc/buildjs-config');
-
-const utils = require('../utils');
-
 const tasks = new Listr([
   {
-    title: 'Linkings packages',
-    task: () => {
-      const dirs = utils.findPackageDirs();
-      const ts = dirs.map(d => ({
-        title: `Building ${d}`,
-        task: () => execa(
-          'webpack',
-          ['--config', 'packages/buildjs/bin/webpack.config.js']),
-      }));
-
-      return new Listr(ts, { concurrent: true });
-    },
+    title: 'Staging Changes',
+    task: () => execa('git', ['add', '.']),
   },
-  // {
-  //   title: 'Staging Changes',
-  //   task: () => execa('git', ['add', '.']),
-  // },
-  // {
-  //   title: 'Lerna Cross Package Publish',
-  //   task: () => execa('lerna', ['publish', '--repo-version', process.env.NEXT_VERSION, '--yes']),
-  // },
+  {
+    title: 'Lerna Cross Package Publish',
+    task: () => execa('lerna', ['publish', '--repo-version', process.env.NEXT_VERSION, '--yes']),
+  },
 ], {
   renderer: UpdaterRenderer,
   collapse: false,
